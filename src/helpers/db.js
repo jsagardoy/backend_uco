298bcc92cd0385cdaea9fd4bcd3ad5dd4bc20a40
Jehú <jehu.sagardoy@gmail.com>
2019-09-17 09:18:59 +0200

    changed localhost

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,10 +1,16 @@
 const config = require('../../config.json');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/ucoDB',{
+
+mongoose.connect('mongodb+srv://user:12345@cluster0-ohrxc.mongodb.net/ucoDB?retryWrites=true&w=majority',{
     useCreateIndex: true,
     useNewUrlParser: true
   });
+
+/* mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/ucoDB',{
+    useCreateIndex: true,
+    useNewUrlParser: true
+  }); */
 mongoose.Promise = global.Promise;
 
 module.exports = {
